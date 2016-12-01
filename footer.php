20ea4935cbedc6055a021b7a8be2eec67adfc08a
Dan Hauk <danhauk@Dans-MacBook-Pro.local>
2016-11-30 19:52:52 -0500

    Initial commit with base _s theme

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package A_Little_Bit_of_Spice
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'a-little-bit-of-spice' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'a-little-bit-of-spice' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'a-little-bit-of-spice' ), 'a-little-bit-of-spice', '<a href="https://danhauk.com" rel="designer">Dan Hauk</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
