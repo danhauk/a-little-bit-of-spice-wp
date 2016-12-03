fcbedb32956dc54173b4acf3f928ef6339e77b18
Dan Hauk <hauk.dan@gmail.com>
2016-12-03 12:49:04 -0500

    header structure

--- header.php
+++ header.php
@@ -20,30 +20,14 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'a-little-bit-of-spice' ); ?></a>
+<div id="theme-container" class="site">
+	<header class="theme-header nav-open" role="banner">
+		<a class="menu-trigger"><span></span></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'a-little-bit-of-spice' ); ?></button>
+		<div class="nav-area">
+			<?php get_search_form(); ?>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</div>
+	</header>
 
 	<div id="content" class="site-content">
---
+++ searchform.php
@@ -0,0 +1,14 @@
+<?php
+/** Custom search form
+*/
+?>
+<div class="widget">
+	<div class="widget_search">
+		<div class="shell">
+			<form action="/" method="GET">
+				<input type="text" value="" autocomplete="off" role="combobox" aria-autocomplete="list" aria-expanded="false" class="react-autosuggest__input" placeholder="Search posts by title or ingredients" id="search" name="s">
+				<button type="submit"><span class="cticon-search"></span></button>
+			</form>
+		</div>
+	</div>
+</div>
