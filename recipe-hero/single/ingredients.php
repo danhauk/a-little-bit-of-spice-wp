71f2e95df47d6dd9d4066165a1c9c801f9523625
Dan Hauk <hauk.dan@gmail.com>
2017-02-02 11:40:13 -0500

    format ingredients list on Recipe Hero output

--- recipe-hero/single/ingredients.php
+++ recipe-hero/single/ingredients.php
@@ -17,15 +17,13 @@ $ingredients = array_filter( $ingredients );
 // Check if any ingredients exist
 if ( isset( $ingredients[0]['name'] ) ) { ?>
 
-	<div class="recipe-single-ingredients">
+	<div class="ing-inline">
 
 		<h4 class="inredients-heading">
 			<?php _e( 'Ingredients', 'recipe-hero' ); ?>
 		</h4>
 
 		<?php do_action( 'recipe_hero_before_ingredients_list' ); ?>
-
-		<ul class="ingredients-list">
 
 		<?php foreach ( (array) $ingredients as $key => $ingredient ) {
 
@@ -36,39 +34,37 @@ if ( isset( $ingredients[0]['name'] ) ) { ?>
 		        $ingredient_quantity = $ingredient['quantity'];
 		        $ingredient_amount_pre = $ingredient['amount'];
 		    	$ingredient_amount = recipe_hero_output_single_ingredient_amount( $ingredient_amount_pre, $ingredient_quantity );
-		  	
+
 		  	}
 
-		    if ( isset( $ingredient['name'] ) ) {  
+		    if ( isset( $ingredient['name'] ) ) {
 
 		        $ingredient_name = $ingredient['name'];
-		        
+
 		    }
 
 		   	?>
 
-		   	<li class="ingredients-item <?php echo rh_format_string( $ingredient_name ); ?>" itemprop="ingredients">
+		   	<div class="ing-line <?php echo rh_format_string( $ingredient_name ); ?>" itemprop="ingredients">
 
 		   		<?php do_action( 'recipe_hero_before_ingredients_list_item' ); ?>
 
-		   		<div class="amount">
-		   			<?php echo $ingredient_amount; ?>
-		   		</div>
-		   		<div class="name">
+		   		<div class="ing-name">
 		   			<?php echo $ingredient_name; ?>
+		   		</div>
+		   		<div class="ing-qty">
+		   			<?php echo $ingredient_amount; ?>
 		   		</div>
 
 		   		<?php do_action( 'recipe_hero_after_ingredients_list_item' ); ?>
 
-		    </li>
+		    </div>
 
 		<?php } ?>
 
 		</ul>
 
 		<?php do_action( 'recipe_hero_after_ingredients_list' ); ?>
-
-	<hr class="recipe-single-seperator" />
 
 	</div><!-- .recipe-single-ingredients -->
 
