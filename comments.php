4e71c304a3c3192c6cb05fd2df2df0c245b271ef
Dan Hauk <hauk.dan@gmail.com>
2017-02-21 00:00:44 -0500

    fix closing tag, remove unnecessary header

--- comments.php
+++ comments.php
@@ -87,18 +87,10 @@ if ( post_password_required() ) {
 				<a data-reactid="218">Leave a comment...</a>
 			</div>
 		</div>
+	</div>
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'a-little-bit-of-spice' ) ),
-					number_format_i18n( get_comments_number() ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			?>
-		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
