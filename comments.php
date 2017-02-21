d9b9cd5bd5adf837ff2f4a3789bf7acc3221049e
Dan Hauk <hauk.dan@gmail.com>
2017-02-20 23:55:45 -0500

    style top of comments section

--- comments.php
+++ comments.php
@@ -20,8 +20,73 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
-
+<div id="comments" class="ct-post-end-section">
+	<div class="ct-post-actions" style="height: 115px;">
+		<div class="ct-post-nav is-visible">
+			<div class="ct-post-nav-container">
+				<div class="ct-post-nav-content">
+					<div class="left">
+						<a href="#" class="follow btn-follow followed acted">
+							<span class="cticon-follow"></span>
+							<span class="btn-text">Following</span>
+						</a>
+					</div>
+					<div class="right">
+						<div class="actions">
+							<a href="javascript:void(0)" class="btn-comment">
+								<span class="cticon-bubbles"></span>
+								<?php if ( have_comments() ) {
+									get_comments_number();
+								} else {
+									echo '0';
+								} ?>
+							</a>
+							<span class="splitter"></span>
+							<?php
+							$urlencode_permalink = urlencode( get_permalink() );
+							if ( has_post_thumbnail() ) {
+								$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
+								if ( ! empty( $large_image_url[0] ) ) {
+									$post_image_url = $large_image_url[0];
+								}
+								else {
+									$post_image_url = '';
+								}
+							}
+							$urlencode_image = urlencode( $post_image_url );
+							$urlencode_excerpt = urlencode( get_the_excerpt() );
+							$urlencode_title = urlencode( get_the_title() );
+							?>
+							<div class="share-icons">
+								<a class="fb" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $urlencode_permalink ?>">
+									<span class="cticon-facebook"></span>
+								</a>
+								<a class="pin" href="http://pinterest.com/pin/create/button/?url=<?php echo $urlencode_permalink; ?>&media=<?php echo $urlencode_image; ?>&description=<?php echo $urlencode_excerpt; ?>">
+									<span class="cticon-pinterest"></span>
+								</a>
+								<a class="share-more">
+									<span class="cticon-more-fill"></span>
+								</a>
+								<a class="tw" href="https://twitter.com/intent/tweet?url=<?php echo $urlencode_permalink ?>">
+									<span class="cticon-twitter"></span>
+								</a>
+								<a class="yum" href="http://www.yummly.com/urb/verify?url=<?php echo $urlencode_permalink; ?>&title=<?php echo $urlencode_title; ?>&image=<?php echo $urlencode_image; ?>">
+									<span class="cticon-yum"></span>
+								</a>
+							</div>
+						</div>
+						<a href="javascript:void(0)" class="share-on-mobile">
+							<span class="cticon-share-alternitive"></span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="comment-block abs">
+			<div class="ct-comment-trigger">
+				<a data-reactid="218">Leave a comment...</a>
+			</div>
+		</div>
 	<?php
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) : ?>
@@ -70,16 +135,6 @@ if ( post_password_required() ) {
 		endif; // Check for comment navigation.
 
 	endif; // Check for have_comments().
-
-
-	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'a-little-bit-of-spice' ); ?></p>
-	<?php
-	endif;
-
-	comment_form();
 	?>
 
 </div><!-- #comments -->
