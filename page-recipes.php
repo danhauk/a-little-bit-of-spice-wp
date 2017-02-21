8d035e41cefabe87a077e58271093a9a7ea34f9c
Dan Hauk <hauk.dan@gmail.com>
2017-02-20 22:01:25 -0500

    pagination on homepage

--- page-recipes.php
+++ page-recipes.php
@@ -26,7 +26,23 @@ get_header(); ?>
 			</div>
 
 		<?php
-		$recipes = new WP_Query( array( 'post_type' => 'recipe', 'posts_per_page' => -1 ) );
+		// set number of posts to display
+		$display_count = 1;
+		// get current page
+		$page = get_query_var( 'page' ) ? get_query_var( 'page' ) : 1;
+		// calculate offset
+		$offset = ( $page - 1 ) * $display_count;
+
+		// query args
+		$recipe_query_args = array(
+			'post_type' => 'recipe',
+			'orderby' => 'date',
+			'order' => 'desc',
+			'posts_per_page' => $display_count,
+			'page' => $page,
+			'offset' => $offset
+		);
+		$recipes = new WP_Query( $recipe_query_args );
 
 		if ( $recipes->have_posts() ) :
 
@@ -44,7 +60,26 @@ get_header(); ?>
 				get_template_part( 'template-parts/content', 'index' );
 
 			endwhile; // End of the loop.
-			wp_reset_query();
+			?>
+
+			<div class="paginate">
+				<?php if ( $page != 1 ): ?>
+				<div class="prev">
+					<a class="btn" href="/page/<?php echo $page-1; ?>">
+						<span class="cticon-back"></span> Newer Posts
+					</a>
+				</div>
+				<?php endif; ?>
+				<?php if ( $page != $recipes->max_num_pages ): ?>
+				<div class="next">
+					<a class="btn" href="/page/<?php echo $page+1; ?>">
+						Older Posts <span class="cticon-front"></span>
+					</a>
+				</div>
+				<?php endif; ?>
+			</div>
+
+			<?php wp_reset_postdata();
 
 		endif; // End if have_posts()
 			?>
