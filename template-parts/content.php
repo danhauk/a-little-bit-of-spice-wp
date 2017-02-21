8af968af8841fc9fbd4cc96455d5777ad10789ff
Dan Hauk <hauk.dan@gmail.com>
2017-02-20 22:37:54 -0500

    add post meta with categories

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,3 +12,18 @@
 <div class="ct-post-content">
 	<?php the_content(); ?>
 </div><!-- #post-## -->
+
+<div class="post-meta">
+	<?php if ( has_category() ): ?>
+	<article>
+		<span>Posted under:</span>
+		<ul class="ct-post-categories">
+			<?php
+			$post_categories = get_the_category();
+			foreach( $post_categories as $category ) {
+				echo "<li><a href='/category/{$category->slug}'>{$category->name}</a></li>";
+			} ?>
+		</ul>
+	</article>
+	<?php endif; ?>
+</div>
