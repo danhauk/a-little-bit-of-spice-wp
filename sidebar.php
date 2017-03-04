2f2c04fd945f9137f4fd8c3b10f3da94b59ce611
Dan Hauk <hauk.dan@gmail.com>
2017-03-03 22:51:44 -0500

    related posts

--- sidebar.php
+++ sidebar.php
@@ -24,5 +24,46 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 		</div>
 
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+		<?php if ( is_single() ) {
+			$cats = wp_get_post_categories( $post->ID );
+			if ( $cats ) {
+				$args = array(
+					'category__in' => $cats,
+					'post__not_in' => array( $post->ID ),
+					'posts_per_page' => 5,
+					'orderby' => 'rand',
+					'ignore_sticky_posts' => true
+				);
+				$related_posts = new WP_Query( $args );
+
+				if ( $related_posts->have_posts() ): ?>
+
+					<div class="similar-posts clearfix">
+						<h3>Similar Posts</h3>
+						<ul class="ct-similar-posts">
+							<?php while ( $related_posts->have_posts() ) : $related_posts->the_post(); ?>
+								<li>
+									<article class="similar-card">
+										<div class="similar-post-wrapper">
+											<div class="img-holder">
+												<a class="thumb img-link" href="<?php the_permalink(); ?>">
+													<?php the_post_thumbnail( 'medium_large' ); ?>
+												</a>
+											</div>
+											<h3>
+												<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+											</h3>
+										</div>
+									</article>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					</div>
+				<?php
+				endif;
+				wp_reset_query();
+			} // if post has categories
+		} // if is_single() ?>
 	</div>
 </aside><!-- #secondary -->
